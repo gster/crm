4080af36119336bc88eeedf3b1ddce11fadb03fe
gster <zg@hntri.com>
2013-01-26 14:15:53 +0800

    移除模板缓存的跟踪

--- Smarty/templates_c/%%97^972^97204CE9%%Footer.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2013-01-21 09:20:12
-         compiled from modules/Mobile/generic/Footer.tpl */ ?>
-</html>
--- Smarty/templates_c/%%E4^E4D^E4DAC3FF%%LoginFooter.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2013-01-21 06:16:28
-         compiled from LoginFooter.tpl */ ?>
-	</div>
-</body>
